0286e0800faf9d792e47838ed3679fd6103d9a65
heeyeoul kim <kheeyeoul@gmail.com>
2023-08-17 11:24:24 +0900

    [update] 생성자 Builder 방식을 클래스 Builder 방식으로 수정

--- src/main/java/com/example/twogether/user/entity/User.java
+++ src/main/java/com/example/twogether/user/entity/User.java
@@ -9,13 +9,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -30,20 +33,10 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
-    @Enumerated(value = EnumType.STRING)
-    private UserRoleEnum role;
-
     private String nickname;
     private String introduction;
 
-    @Builder
-    public User(String email, String password, UserRoleEnum role, String nickname,
-        String introduction) {
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.nickname = nickname;
-        this.introduction = introduction;
-    }
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
 }
