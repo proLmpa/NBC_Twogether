05a766342c5f9d85f219ad50269a40889debc1b3
YoungDevSpace <iamsoyoungdev@gmail.com>
2023-08-22 02:04:01 +0900

    Revert "[update] 보드에서 덱 조회"
    This reverts commit fdc953684a3fb9f5ed1b48c1aa98cb100d7d3488.

--- src/main/java/com/example/twogether/board/dto/BoardResponseDto.java
+++ src/main/java/com/example/twogether/board/dto/BoardResponseDto.java
@@ -1,10 +1,13 @@
 package com.example.twogether.board.dto;
 
 import com.example.twogether.board.entity.Board;
-import com.example.twogether.deck.dto.DeckResponseDto;
+import com.example.twogether.common.dto.ApiResponseDto;
+import com.example.twogether.workspace.dto.WpColResponseDto;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
@@ -15,7 +18,6 @@ public class BoardResponseDto {
     private String color;
     private String info;
     private List<BoardColResponseDto> boardCollaborators;
-    private List<DeckResponseDto> decks;
 
     public static BoardResponseDto of(Board board) {
         return BoardResponseDto.builder()
@@ -26,7 +28,6 @@ public class BoardResponseDto {
             .info(board.getInfo())
             .boardCollaborators(board.getBoardCollaborators().stream().map(
                 BoardColResponseDto::of).toList())
-            .decks(board.getDecks().stream().map(decks -> new DeckResponseDto(decks)).toList())
             .build();
     }
 }
