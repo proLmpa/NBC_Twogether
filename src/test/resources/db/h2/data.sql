17d64c37712c8ef78d3756143bddf25e7b64b33e
Han JiHun <hjh_3229@naver.com>
2023-08-29 15:41:47 +0900

    [update] 덱 테스트 코드 덱 생성 성공 확인

--- src/test/resources/db/h2/data.sql
+++ src/test/resources/db/h2/data.sql
@@ -1,6 +1,6 @@
 -- users 테이블 생성
-CREATE TABLE users (
-    id bigint PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+    id LONG PRIMARY KEY,
     email VARCHAR(100),
     password VARCHAR(100),
     introduction VARCHAR(100),
@@ -15,11 +15,11 @@ INSERT INTO users (id, email, introduction, nickname, password, role) VALUES
 (3, 'user3@mail.com', 'tester', 'tester', 'test1234!', 'USER');
 
 -- workspace 테이블 생성
-CREATE TABLE workspace (
-    id bigint PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS workspace (
+    id LONG PRIMARY KEY,
     title VARCHAR(50),
     icon VARCHAR(50),
-    user_id bigint,
+    user_id LONG,
     created_at datetime,
     modified_at datetime,
     FOREIGN KEY (user_id) REFERENCES users(id)
@@ -27,18 +27,18 @@ CREATE TABLE workspace (
 
 -- 더미 workspace 데이터 삽입
 INSERT INTO workspace (id, title, icon, user_id, created_at, modified_at) VALUES
-(1, 'Workspace 1', 'test', 1, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000'),
-(2, 'Workspace 2', 'test', 1, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000'),
-(3, 'Workspace 1', 'test', 2, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000');
+(1, 'Workspace 1', 'test', 1, '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
+(2, 'Workspace 2', 'test', 1, '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
+(3, 'Workspace 1', 'test', 2, '2023-01-01 00:00:00', '2023-01-01 00:00:00');
 
 -- board 테이블 생성
-CREATE TABLE board (
-    id bigint PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS board (
+    id LONG PRIMARY KEY,
     title VARCHAR(50),
     color VARCHAR(50),
     info VARCHAR(50),
-    workspace_id bigint,
-    user_id bigint,
+    workspace_id LONG,
+    user_id LONG,
     created_at datetime,
     modified_at datetime,
     FOREIGN KEY (workspace_id) REFERENCES workspace(id),
@@ -47,17 +47,17 @@ CREATE TABLE board (
 
 -- 더미 board 데이터 삽입
 INSERT INTO board (id, title, color, info, user_id, workspace_id, created_at, modified_at) VALUES
-(1, 'Board 1', 'black', 'test', 1, 1, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000'),
-(2, 'Board 2', 'white', 'test', 1, 1, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000'),
-(3, 'Board 1', 'black', 'test', 1, 2, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000');
+(1, 'Board 1', 'black', 'test', 1, 1, '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
+(2, 'Board 2', 'white', 'test', 1, 1, '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
+(3, 'Board 1', 'black', 'test', 1, 2, '2023-01-01 00:00:00', '2023-01-01 00:00:00');
 
 -- deck 테이블 생성
-CREATE TABLE deck (
-    id bigint PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS deck (
+    id LONG PRIMARY KEY,
     title VARCHAR(50),
     position float,
     is_archived bit,
-    board_id bigint,
+    board_id LONG,
     created_at datetime,
     modified_at datetime,
     FOREIGN KEY (board_id) REFERENCES board(id)
@@ -65,21 +65,21 @@ CREATE TABLE deck (
 
 -- 더미 deck 데이터 삽입
 INSERT INTO deck (id, title, position, is_archived, board_id, created_at, modified_at) VALUES
-(1, 'Deck 1', 128, false, 1, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000'),
-(2, 'Deck 2', 256, false, 1, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000'),
-(3, 'Deck 3', 384, false, 1, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000'),
-(3, 'Deck 3', 512, true, 1, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000');
+(1, 'Deck 1', 128, false, 1, '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
+(2, 'Deck 2', 256, false, 1, '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
+(3, 'Deck 3', 384, false, 1, '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
+(4, 'Deck 3', 512, true, 1, '2023-01-01 00:00:00', '2023-01-01 00:00:00');
 
 -- card 테이블 생성
-CREATE TABLE card (
-    id bigint PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS card (
+    id LONG PRIMARY KEY,
     title VARCHAR(50),
     description VARCHAR(50),
     attachment VARCHAR(50),
     position float,
     is_archived bit,
     due_date datetime,
-    deck_id bigint,
+    deck_id LONG,
     created_at datetime,
     modified_at datetime,
     FOREIGN KEY (deck_id) REFERENCES deck(id)
@@ -87,9 +87,9 @@ CREATE TABLE card (
 
 -- 더미 card 데이터 삽입
 INSERT INTO card (id, title, description, attachment, position, is_archived, due_date, deck_id, created_at, modified_at) VALUES
-(1, 'Card 1-1', 'test', null, 128, false, null, 1, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000'),
-(2, 'Card 1-2', 'test', null, 256, false, null, 1, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000'),
-(3, 'Card 1-3', 'test', null, 384, false, null, 1, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000'),
-(4, 'Card 2-1', 'test', null, 512, false, null, 2, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000'),
-(5, 'Card 2-2', 'test', null, 640, false, null, 2, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000'),
-(6, 'Card 3-1', 'test', null, 768, true, null, 3, '2023-01-0100-00-00.000', '2023-01-0100-00-00.000');
+(1, 'Card 1-1', 'test', null, 128, false, null, 1, '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
+(2, 'Card 1-2', 'test', null, 256, false, null, 1, '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
+(3, 'Card 1-3', 'test', null, 384, false, null, 1, '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
+(4, 'Card 2-1', 'test', null, 512, false, null, 2, '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
+(5, 'Card 2-2', 'test', null, 640, false, null, 2, '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
+(6, 'Card 3-1', 'test', null, 768, true, null, 3, '2023-01-01 00:00:00', '2023-01-01 00:00:00');
