5e9e0995ea95fd653db3feebaf789d0aa19f6448
song eojin <feshed528@gmail.com>
2023-08-31 00:59:11 +0900

    [update] SSE 관련 파일 모두 삭제

--- src/main/java/com/example/twogether/alarm/service/AlarmService.java
+++ src/main/java/com/example/twogether/alarm/service/AlarmService.java
@@ -25,7 +25,6 @@ public class AlarmService {
     @Transactional(readOnly = true)
     public AlarmsResponseDto getAlarms(User user) {
 
-        //if(boardColRepository.existsByUser_IdOrBoardCollaborator_IdAndBoard_Id(user.getId(), boardId)) {
         List<Alarm> alarms = alarmRepository.findAllByReceiver(user);
         return AlarmsResponseDto.of(alarms);
     }
@@ -46,62 +45,4 @@ public class AlarmService {
 
         alarmRepository.delete(alarm);
     }
-
-//    private final EmitterRepository emitterRepository;
-//
-//    private static final Long SSE_END_TIME = (60 * 60 * 1000) * 3L;
-//
-//    // 로그인 후 알림 구독을 위해 필요한 emitter 생성
-//    public SseEmitter subscribe(Long userId, String lastEventId) {
-//        String emitterId = createIdByUserId(userId);
-//        SseEmitter emitter = emitterRepository.saveEmitter(emitterId, new SseEmitter(SSE_END_TIME));
-//
-//        emitter.onCompletion(() -> emitterRepository.deleteById(emitterId));
-//        emitter.onTimeout(() -> emitterRepository.deleteById(emitterId));
-//        emitter.onError((e) -> emitterRepository.deleteById(emitterId));
-//        emitterRepository.saveEmitter(emitterId, emitter);
-//
-//        // 503 에러를 방지하기 위한 더미 이벤트 전송
-//        String eventId = createIdByUserId(userId);
-//        sendNotification(emitter, eventId, emitterId, "EventStream Created. [userId=" + userId + "]");
-//
-//        // 클라이언트가 미수신한 Event 목록이 존재할 경우 전송하여 Event 유실을 예방
-//        if (hasLostData(lastEventId)) {
-//            sendLostData(lastEventId, userId, emitterId, emitter);
-//        }
-//
-//        return emitter;
-//    }
-//
-//    public void sendAlarm(User receiver, AlarmTrigger alarmTrigger, String url) {
-//        Alarm alarm = alarmRepository.save(new Alarm(receiver, alarmTrigger, url));
-//        String receiverId = String.valueOf(receiver.getId());
-//
-//        Map<String, SseEmitter> sseEmitters = emitterRepository.findAllEmitterStartWithByUserId(receiverId);
-//        sseEmitters.forEach(
-//            (key, emitter) -> {
-//                emitterRepository.saveEvent(key, alarm);
-//                sendToClient(emitter, key, AlarmResponseDto.of(alarm));
-//            }
-//        );
-//    }
-//
-//    private void sendToClient(SseEmitter emitter, String emitterId, Object data) {
-//        try {
-//            emitter.send(SseEmitter.event()
-//                .id(emitterId)
-//                .data(data));
-//        } catch (IOException exception) {
-//            emitterRepository.deleteById(emitterId);
-//            throw new CustomException(CustomErrorCode.UNINVITED_BOARD);
-//        }
-//    }
-//
-//    private String createIdByUserId(Long userId) {
-//        return userId + "_" + System.currentTimeMillis();
-//    }
-//
-//    private boolean hasLostData(String lastEventId) {
-//        return !lastEventId.isEmpty();
-//    }
 }
