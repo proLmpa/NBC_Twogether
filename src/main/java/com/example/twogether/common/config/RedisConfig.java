ce8f462d0b158851574858a7b6333d3a022745b8
heeyeoul kim <kheeyeoul@gmail.com>
2023-09-06 19:49:23 +0900

    [update] 프론트 이메일 인증 fetch api 경로와 메서드 타입 수정

--- src/main/java/com/example/twogether/common/config/RedisConfig.java
+++ src/main/java/com/example/twogether/common/config/RedisConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
+@EnableRedisRepositories(basePackages = "com.example.twogether.common.redis")
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
